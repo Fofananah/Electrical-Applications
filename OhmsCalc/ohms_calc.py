9741573e87ae0980208e21b9ce6b66cd766d44aa
Fofananah <a.andrew.fofanah@gmail.com>
2025-12-13 22:44:32 -0600

    input response upload

--- OhmsCalc/ohms_calc.py
+++ OhmsCalc/ohms_calc.py
@@ -1,1 +1,16 @@
 print("Ohm's Law Calculator")
+
+intensity_input = input("Enter the current intensity (I) In Amperes.")
+I = float(intensity_input)
+
+resistance_input = input("Enter the resistance (R) in Ohms.")
+R = float(resistance_input)
+
+#Ohm's Law V = I * R
+V = I * R
+
+print("***********************************")
+print(f"Current (I): {I} A")
+print(f"Resistance (R): {R} Ω")
+print(f"Calculated Voltage (V): {V} V")
+print("***********************************")

51dc95891049f8d7bb3251a93038013e70473d6b
Fofananah <a.andrew.fofanah@gmail.com>
2025-12-15 21:19:18 -0600

    Parallel Resistance inclusion

--- Digital Circuit/dc_core.py
+++ Digital Circuit/dc_core.py
@@ -7,8 +7,17 @@ class Resistor:
 def series_resistance(resistors):
     if not resistors:
         raise ValueError("No resistors in circuit.")
-    total = 0.0
     
+    total = 0.0
     for r in resistors:
         total += r.R
     return total
+
+def parallel_resistance(resistors):
+    if not resistors:
+        raise ValueError("No resistors in circuit.")
+    
+    inverse_total = 0.0
+    for r in resistors:
+        inverse_total += 1 / r.R 
+    return 1 / inverse_total
--- Digital Circuit/runtest.py
+++ Digital Circuit/runtest.py
@@ -1,8 +1,10 @@
-from dc_core import Resistor, series_resistance
+from dc_core import Resistor, series_resistance, parallel_resistance
 
 potato = Resistor(5000)
 tomato = Resistor(2000)
 
 req = series_resistance([potato,tomato])
-
 print("Potato Tomato:", req)
+
+req_parallel = parallel_resistance([potato,tomato])
+print("Chopped!", req_parallel)

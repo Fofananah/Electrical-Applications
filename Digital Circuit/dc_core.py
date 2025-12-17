39fec394860cba0c0ada518bd8f07ed2bed67a92
Fofananah <a.andrew.fofanah@gmail.com>
2025-12-17 01:52:48 -0600

    Voltage, Current, & Circuit class init
    New class entry

--- Digital Circuit/dc_core.py
+++ Digital Circuit/dc_core.py
@@ -2,7 +2,22 @@ class Resistor:
     def __init__(self, resistance_ohms):
         if resistance_ohms <=0:
             raise ValueError("Resistance must be positive.")
-        self.R = resistance_ohms
+        self.resistance = resistance_ohms
+
+class VoltageSource:
+    def __init__(self,voltage_volts):
+        self.voltage = voltage_volts
+        
+class CurrentSource:
+    def __init__(self,current_amps):
+        self.current = current_amps
+
+
+class Circuit:
+    def __init__(self):
+        self.components = []
+        self.nodes = set()
+
 
 def series_resistance(resistors):
     if not resistors:
@@ -10,7 +25,7 @@ def series_resistance(resistors):
     
     total = 0.0
     for r in resistors:
-        total += r.R
+        total += r.resistance
     return total
 
 def parallel_resistance(resistors):
@@ -19,5 +34,5 @@ def parallel_resistance(resistors):
     
     inverse_total = 0.0
     for r in resistors:
-        inverse_total += 1 / r.R 
+        inverse_total += 1 / r.resistance 
     return 1 / inverse_total

bcce83d9696c9b8a716d5b2879e66c16cb704edb
Fofananah <a.andrew.fofanah@gmail.com>
2025-12-18 04:29:38 -0600

    Circuit node map method init, node instance init
    Run test update into series circuit with node structure.

--- Digital Circuit/dc_core.py
+++ Digital Circuit/dc_core.py
@@ -1,38 +1,59 @@
 class Resistor:
-    def __init__(self, resistance_ohms):
+    def __init__(self, resistance_ohms, a_node,z_node):
         if resistance_ohms <=0:
             raise ValueError("Resistance must be positive.")
         self.resistance = resistance_ohms
+        #Resistor entrance and exit
+        self.a_node = a_node
+        self.z_node = z_node
 
 class VoltageSource:
-    def __init__(self,voltage_volts):
-        self.voltage = voltage_volts
+    def __init__(self,voltage_volts,pos_node,neg_node):
+        self.voltage = voltage_volts   
+        #Voltage source postive, and negative terminals   
+        self.pos_node = pos_node
+        self.neg_node= neg_node
         
 class CurrentSource:
-    def __init__(self,current_amps):
-        self.current = current_amps
-
+    def __init__(self,current_amps,from_node,to_node):
+        self.current = current_amps      
+        #Current flow direction 
+        self.from_node = from_node
+        self.to_node = to_node
 
 class Circuit:
     def __init__(self):
         self.components = []
+        #set to create unique ID's for each node
         self.nodes = set()
-
-
+        
+    def add_components(self,component):
+        self.component.append(component)
+               
+        if isinstance(component,Resistor):
+            self.nodes.add(component.a_node)
+            self.nodes.add(component.z_node)
+        elif isinstance(component,VoltageSource):
+            self.nodes.add(component.pos_node)
+            self.nodes.add(component.neg_node)
+        elif isinstance(component,CurrentSource):
+            self.nodes.add(component.from_node)
+            self.nodes.add(component.to_node)
+            
 def series_resistance(resistors):
     if not resistors:
         raise ValueError("No resistors in circuit.")
     
-    total = 0.0
+    series_total = 0.0
     for r in resistors:
-        total += r.resistance
-    return total
+        series_total += r.resistance
+    return series_total
 
 def parallel_resistance(resistors):
     if not resistors:
         raise ValueError("No resistors in circuit.")
     
-    inverse_total = 0.0
+    parallel_total = 0.0
     for r in resistors:
-        inverse_total += 1 / r.resistance 
-    return 1 / inverse_total
+        parallel_total += 1 / r.resistance 
+    return 1 / parallel_total
--- Digital Circuit/runtest.py
+++ Digital Circuit/runtest.py
@@ -1,7 +1,7 @@
 from dc_core import Resistor, series_resistance, parallel_resistance
 
-potato = Resistor(5000)
-tomato = Resistor(2000)
+tomato = Resistor(5000, 1,2)
+potato = Resistor(2000, 2,0)
 
 req = series_resistance([potato,tomato])
 print("Potato,Tomato?", req,'Ω')

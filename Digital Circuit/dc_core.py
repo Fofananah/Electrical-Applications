dd427e726821e77e1bc3cbf35dc95b4db5390231
Fofananah <a.andrew.fofanah@gmail.com>
2025-12-18 20:49:33 -0600

    Init Current Intensity calc, & Voltage Library
    Runtest Update

--- Digital Circuit/dc_core.py
+++ Digital Circuit/dc_core.py
@@ -3,32 +3,34 @@ class Resistor:
         if resistance_ohms <=0:
             raise ValueError("Resistance must be positive.")
         self.resistance = resistance_ohms
-        #Resistor entrance and exit
+        # Resistor entrance and exit
         self.a_node = a_node
         self.z_node = z_node
 
 class VoltageSource:
     def __init__(self,voltage_volts,pos_node,neg_node):
         self.voltage = voltage_volts   
-        #Voltage source postive, and negative terminals   
+        # Voltage source postive, and negative terminals   
         self.pos_node = pos_node
         self.neg_node= neg_node
         
 class CurrentSource:
     def __init__(self,current_amps,from_node,to_node):
         self.current = current_amps      
-        #Current flow direction 
+        # Current flow direction 
         self.from_node = from_node
         self.to_node = to_node
 
 class Circuit:
     def __init__(self):
         self.components = []
-        #set to create unique ID's for each node
+        # set to create unique ID's for each node
         self.nodes = set()
+        # voltage dictionary for each node
+        self.node_voltages = {}
         
     def add_components(self,component):
-        self.component.append(component)
+        self.components.append(component)
                
         if isinstance(component,Resistor):
             self.nodes.add(component.a_node)
@@ -40,6 +42,21 @@ class Circuit:
             self.nodes.add(component.from_node)
             self.nodes.add(component.to_node)
             
+    # current intensity calculation  I = V / R
+    def get_resistor_current(self, resistor):
+        v_start = self.node_voltages.get(resistor.a_node, 0.0)
+        v_end = self.node_voltages.get(resistor.z_node, 0.0)
+        v_total = v_start - v_end
+        currentIntensity = v_total / resistor.resistance
+        return currentIntensity
+
+    # auto sets node voltage based on voltage source
+    def apply_voltage(self):
+        for comp in self.components:
+            if isinstance(comp, VoltageSource):
+                self.node_voltages[comp,pos_node] = comp.voltage
+                self.node_voltages[comp,neg_node] = 0.0
+        
 def series_resistance(resistors):
     if not resistors:
         raise ValueError("No resistors in circuit.")
--- Digital Circuit/runtest.py
+++ Digital Circuit/runtest.py
@@ -1,10 +1,31 @@
-from dc_core import Resistor, series_resistance, parallel_resistance
+from dc_core import Resistor, VoltageSource, Circuit, CurrentSource, series_resistance, parallel_resistance
 
-tomato = Resistor(5000, 1,2)
-potato = Resistor(2000, 2,0)
+friendship = VoltageSource(9001, 1,0)
+potato = Resistor(5000, 1,2)
+tomato = Resistor(2000, 2,0)
 
-req = series_resistance([potato,tomato])
-print("Potato,Tomato?", req,'Ω')
+print("="*40)
+print("↺ RUNTEST SIMULATION ↻")
+print("="*40)
 
-req_parallel = parallel_resistance([potato,tomato])
-print("Chopped!", req_parallel,'Ω')
+print("Components List")
+print("----------------")
+print("POWER SOURCE:")
+print(f"Friendship: {friendship.voltage}|V")
+print("\nRESISTORS:")
+print(f"Potato: {potato.resistance}|Ω, Entrance: {potato.a_node} Exit: {potato.z_node}")
+print(f"Tomato: {tomato.resistance}|Ω, Entrance: {tomato.a_node} Exit: {tomato.z_node}")
+
+print("\n"+"="*40)
+print("Circuit Structure")
+print("="*40)
+
+print("\nIn Parallel:")
+print(f"Friendship splits at {friendship.pos_node}, "
+      f"to power Potato and Tomato, and then returns back to friendship at {friendship.neg_node}.")
+
+print("\nIn Series:")
+print(f"Friendship goes through Potato, from {friendship.pos_node}, "
+      f"to {potato.z_node}, then through Tomato from {potato.z_node} to {friendship.neg_node}, "
+      f"\nthrough friendship 0 becomes 1.")
+print("="*40)
